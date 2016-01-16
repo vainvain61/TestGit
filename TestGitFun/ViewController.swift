f4709fe1c9679974082cb0fc2ddae0b110b3f237
slambert <slambert@mac.com>
2016-01-15 22:37:00 -0500

    Initial Commit

---
+++ TestGitFun/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TestGitFun
+//
+//  Created by Sylvain on 2016-01-15.
+//  Copyright © 2016 Sylvain Lambert. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
